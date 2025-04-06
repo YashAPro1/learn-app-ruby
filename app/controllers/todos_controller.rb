323efda00db6f8889c4ea55487cacc327c7476ed
YashAPro1 <rekhasd01000001@gmail.com>
2025-04-06 20:47:45 +0530

    Multi-user Todo App with Devise and PostgreSQL on Docker

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,66 @@
+class TodosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_todo, only: %i[show edit update destroy]
+
+  def index
+    @todos = current_user.todos
+  end
+
+
+
+  # GET /todos/1 or /todos/1.json
+  def show
+  end
+
+  # GET /todos/new
+  def new
+    @todo = Todo.new
+  end
+
+  # GET /todos/1/edit
+  def edit
+  end
+
+  # POST /todos or /todos.json
+  def create
+    @todo = current_user.todos.build(todo_params)
+    if @todo.save
+      redirect_to todos_path, notice: "Todo created"
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /todos/1 or /todos/1.json
+  def update
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.html { redirect_to @todo, notice: "Todo was successfully updated." }
+        format.json { render :show, status: :ok, location: @todo }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /todos/1 or /todos/1.json
+  def destroy
+    @todo.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to todos_path, status: :see_other, notice: "Todo was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = current_user.todos.find(params[:id])
+    end
+    
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
+end
